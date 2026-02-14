919e216d7822f5161768d465111b0c7c151d1ac9
BobinaLiza <bobinaliza14@gmail.com>
2026-02-14 10:56:23 +0300

    Add greeting text

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -14,6 +15,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +23,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             HappyBirthdayTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    GreetingText(
+                        message = "Happy Birthday Liza!",
+                        from = "From Emma"
+                    )
                 }
             }
         }
@@ -28,16 +34,21 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingText(message: String, modifier: Modifier = Modifier) {
-    Text(text = message,
-        fontSize = 100.sp,
-        lineHeight = 116.sp,
-    )
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Row {
+        Text(text = message,
+            fontSize = 30.sp,
+            lineHeight = 116.sp
+        )
+        Text(
+            text=from,
+            fontSize = 36.sp)
+    }
 }
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingText(message = "Happy Birthday Liza!")
+        GreetingText(message = "Happy Birthday Liza!", from = "From Emma")
     }
 }
